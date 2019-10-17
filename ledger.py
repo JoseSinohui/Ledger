97e88cf1c9e6565807ecb8212d8ba87c131612bd
Jose Sinohui <jsinohui@nearsoft.com>
2019-10-17 15:06:26 -0700

    Adds simple reg impl

--- ledger.py
+++ ledger.py
@@ -45,26 +45,61 @@ def handleFile(filePath, transactions):
                 splittedLine = [x.strip() for x in line.replace("\t", '', line.count('\t') - 1).split('\t')]
 
                 account = splittedLine[0];
-                amount = splittedLine[1] if len(splittedLine) > 1 else "Pending"
-                
+                amountStr = splittedLine[1].strip() if len(splittedLine) > 1 else "Pending"
+                amount = 0
 
-                if defaultCurrency in amount:
-                    amount = float(amount.replace(defaultCurrency, ''))
-
-
-                # if amount == '':
-                #     amount = transaction.postings[0]
-                #     for pos in transaction.postings[1:]:
-                #         amount += pos.amount;
+                if defaultCurrency in amountStr:
+                    amount = float(amountStr.replace(defaultCurrency, ''))
+                    currency = defaultCurrency
+                else:
+                    if len(amountStr.split(" ")) > 1:
+                        amount = float(amountStr.split(" ")[0])
+                        currency = amountStr.split(" ")[1]
+                    else:
+                        currency = defaultCurrency
+            
+                if amount == 0:
+                    amount = transaction.postings[0].amount
+                    for pos in transaction.postings[1:-1]:
+                        amount += pos.amount;
+                    amount = -amount
 
                 posting.account = account
                 posting.amount = amount
+                posting.currency = currency
                 accounts -=1
         
+def register(regex, transtions):
+    currencies = {};
+    for transaction in transactions:
+        print(str(transaction.date) + " " + transaction.description, end='\t')
+        for posting in transaction.postings:
+            print(posting.account, end="\t")
+            print(str(posting.amount) + posting.currency, end="\t")
+
+            # Apply operation
+            if posting.currency not in currencies:
+                currencies[posting.currency] = 0
+            
+            currencies[posting.currency] += posting.amount
+            
+            # Print balances
+            for curr, amnt in currencies.items():
+                if amnt != 0:
+                    print(str(amnt) + curr)
+            
+            if(all(value == 0 for value in currencies.values())):
+                print("0")
+        
+
+
 transactions = []
 filePath = sys.argv[1]
 handleFile(filePath, transactions)
-print("Done")
+
+
+register("", transactions)
+
 
 
 
